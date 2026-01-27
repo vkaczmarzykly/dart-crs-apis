86a80798742bc1392ff8f0a0cae92fefec603df5
vkaczmarzykly <141168375+vkaczmarzykly@users.noreply.github.com>
2026-01-27 11:26:00 -0300

    aula02

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,9 @@
 import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() {
-  requestData();
+  //requestData();
+  requestDataAsync();
 }
 
 Future<void> requestData() async {
@@ -10,5 +12,16 @@ Future<void> requestData() async {
   Future<Response> futureResponse = get(uri);
   futureResponse.then((response) {
     print(response.body);
+    List<dynamic> listAccounts = jsonDecode(response.body);
+    Map<String, dynamic> mapCarla = listAccounts.firstWhere(
+      (element) => element['name'] == 'Carla',
+    );
+    print(mapCarla["balance"]);
   });
+}
+
+dynamic requestDataAsync() async {
+  String url = 'https://gist.githubusercontent.com/vkaczmarzykly/c623ab0fff954574e1a1921f2ee542a2/raw/b51b66f7ab32073c375622c23daaaae2965a0dc4/accounts.json';
+  Response response = await get(Uri.parse(url));
+  print(json.decode(response.body)[0]);
 }
